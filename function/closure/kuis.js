9d7b9b1f4b19d406f1b418b377ec9fc30453d1a1
WahidinAji <a17wahidin@gmail.com>
2021-05-20 14:05:04 +0700

    kuis function

--- function/closure/kuis.js
+++ function/closure/kuis.js
@@ -6,7 +6,7 @@
 // }
 //cara 2 pakai arrow function
 const minimal = (a,b) => a==b ? a : a < b ? a : b;
-
 const power = (a,b) => Math.pow(a,b);
+
 console.log(minimal(4,1));
 console.log(power(7,3));
