0552649d509d8b7faa873924673b396a74a4b925
WahidinAji <a17wahidin@gmail.com>
2021-05-20 14:04:49 +0700

    kuis function

---
+++ function/closure/kuis.js
@@ -0,0 +1,12 @@
+//cara 1 pakai regular function
+// function minimal(a,b) {
+//     return a == b ? a
+//          : a<b ? a
+//          : b;
+// }
+//cara 2 pakai arrow function
+const minimal = (a,b) => a==b ? a : a < b ? a : b;
+
+const power = (a,b) => Math.pow(a,b);
+console.log(minimal(4,1));
+console.log(power(7,3));
