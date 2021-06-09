bdec6863f1af99ae4cb6948de36f746d54d29ade
WahidinAji <a17wahidin@gmail.com>
2021-06-09 09:39:45 +0700

    test.js versi es6, index versi funciton biasa. node: index.js sudah submit kuis

--- concurrency/index.js
+++ concurrency/index.js
@@ -6,7 +6,7 @@ class NetworkError extends Error {
 }
 
 // TODO: 1
-const fetchingUserFromInternet = (isOffline) => {
+const fetchingUserFromInternet = isOffline => {
   return new Promise((resolve,reject) => {
     setTimeout(() => {
       if (isOffline) {
@@ -18,16 +18,13 @@ const fetchingUserFromInternet = (isOffline) => {
   });
 };
 
-
-
 // TODO: 2
-const  gettingUserName = async() => {
-  try{
-    const user = await fetchingUserFromInternet(false)
-    console.log(user.name);
-  } catch(rejectReason) {
-    console.log(rejectReason.message);
-  }
+async function gettingUserName(){
+    try {
+        const user = await fetchingUserFromInternet(false);
+        return user.name;
+    } catch(rejectedReason) {
+        return rejectedReason.message;
+    }
 };
-
 gettingUserName();
--- concurrency/test.js
+++ concurrency/test.js
@@ -1,35 +1,30 @@
 class NetworkError extends Error {
     constructor(message) {
-        super(message);
-        this.name = 'NetworkError';
+      super(message);
+      this.name = 'NetworkError';
     }
 }
 
 // TODO: 1
-const fetchingUserFromInternet = (isOffline) => {
-    return new Promise((resolve,reject) => {
+const fetchingUserFromInternet = isOffline => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (isOffline) {
-                reject(new NetworkError('Gagal mendapatkan data dari internet'));
-            } else {
+                reject(new NetworkError('Gagal mendapatkan data dari internet'), null);
+            }else {
                 resolve({ name: 'John', age: 18 });
             }
         }, 500);
-    });
+    })
 };
-const handleSuccess = resolvedValue => {
-    console.log(resolvedValue.name);
-}
-const handleFailure = rejectionReason => {
-    console.log(rejectionReason);
-}
-
 
 // TODO: 2
-const gettingUserName = () => {
-    fetchingUserFromInternet(false)
-        .then(handleSuccess)
-        .catch(handleFailure);
-};
-
+const gettingUserName = async() => {
+    try{
+        const user = await fetchingUserFromInternet(true);
+        console.log(user.name);
+    } catch(rejectedReason) {
+        console.log(rejectedReason.message);
+    }
+}
 gettingUserName();
